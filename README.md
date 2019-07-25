ae6f7b86b9590380ea6cbf41e6af4751fe27ef1c
George Adams IV (geowa4) <oss@geowa4.software>
2019-07-24 22:55:02 -0400

    document and license initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Learn Loki
+
+```bash
+vagrant up
+vagrant ssh
+cd demo
+docker-compose pull
+docker-compose up
+```
--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
 mkdir -p "$HOME"/loki/{chunks,index}
 mkdir -p "$HOME"/promtail
-# cd "$HOME"/demo
-# docker-compose pull
-# docker-compose up
